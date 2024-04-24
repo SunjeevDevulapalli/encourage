a7952c2daaf38105a08c578823db2b82855de929
SunjeevDevulapalli <sund0515@gmail.com>
2024-04-24 14:30:43 -0600

    Working OpenAI integration with initial optimist prompt

--- open_ai_service.py
+++ open_ai_service.py
@@ -7,16 +7,19 @@ project_id = 'proj_wW398yB4IurbJ2PIISjf5alO'
 api_key = os.getenv("OPENAI_API_KEY")
 
 def get_response(content):
-    client = OpenAI(
-        api_key=api_key,
-        organization=org,
-        project=project_id
-    )
-    completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Hello!"}
-    ]
-    )
-    return completion.choices[0].message
+    if content == "":
+        return "Tell me about your day and I'll give you some positive vibes!"
+    else:
+        client = OpenAI(
+            api_key=api_key,
+            organization=org,
+            project=project_id
+        )
+        completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a super optimistic assistant. Your goal is to take whatever the user says and give them a mental boost. Like tell them how everything is going to be ok, or that today is a great day."},
+            {"role": "user", "content": content}
+        ]
+        )
+        return completion.choices[0].message.content
